2f9026b004bb85c8a417a4bea5b75dd71b63559c
root <frrrredo@gmail.com>
2016-01-16 02:36:39 +0400

    stable

--- mac-correter.sh
+++ mac-correter.sh
@@ -12,26 +12,21 @@ if [[ $mac_raw == *[:]* ]]
 then
    # "MAC Contans :" 
    # check MAC using regex:
-   if [[ "mac_raw" =~ "^([a-fA-F0-9]{2}:){5}[a-zA-Z0-9]{2}$" ]]  && [[ ${#mac_raw} -eq 17 ]];
+   if [[ $mac_raw =~ ^([a-fA-F0-9]{2}:){5}[a-zA-Z0-9]{2}$ ]] && [[ ${#mac_raw} -eq 17 ]];
    then 
          # valid
          mac_address=$(echo "$mac_raw" | tr [:upper:] [:lower:])
    else 
          # invalid
-         echo "Entered MAC adrress is invalid: $mac_raw"
+         echo "1 Entered MAC adrress is invalid: $mac_raw"
          exit 1;
    fi
 fi 
-
     #MAC Without ":" validate characters
-    if  [[ $mac_raw =~ [^[:digit:][alpha]] ]] && [[ ${#mac_raw} -eq 12 ]];
+    if  [[ $mac_raw =~ [^[:digit:][:alpha:]] ]] && [[ ${#mac_raw} -eq 12 ]];
      then 
          # valid and set format
          mac_address=$( echo $mac_raw | sed 's/.\{2\}/&:/g' | tr [:upper:] [:lower:])
-    else 
-         # invalid
-         echo "Entered MAC adrress is invalid: $mac_raw"
-         exit 1;
 
 fi
 
